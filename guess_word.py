4bd99d525e8e86b72c5b218788d1d87b38aa887b
Techiee Guy <92099128+techieeguy@users.noreply.github.com>
2021-10-08 18:29:49 +0530

    Guess Word Game in Python

---
+++ guess_word.py
@@ -0,0 +1,46 @@
+import random
+
+name = input("What is your name? - ")
+
+print("Good Luck ! ", name)
+
+words = ['java', 'computer', 'tech', 'programming',
+		'python', 'techieeguy', 'coding', 'learning',
+		'share', 'like', 'follow']
+
+word = random.choice(words)
+
+
+print("Guess the characters...")
+
+guesses = ''
+
+turns = 12
+
+while turns > 0:
+	failed = 0
+	
+	for char in word:
+		if char in guesses:
+			print(char)
+			
+		else:
+			print("_")
+			failed += 1
+	
+	if failed == 0:
+		print("You Win!")
+		print("The word is: ", word)
+		break
+	
+	guess = input("Guess a character: ")
+	
+	guesses += guess
+	
+	if guess not in word:
+		turns -= 1
+		print("Wrong!")
+		print("You have", + turns, 'more guesses.')
+		
+		if turns == 0:
+			print("You Loose!")
